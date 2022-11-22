bf4cb2bbe9b27132d8f343b6712ed9a901e72c9a
Dave <178698695@qq.com>
2022-11-22 18:15:32 +0800

    ads

--- apiphalcon/apps/frontend/controllers/IndexController.php
+++ apiphalcon/apps/frontend/controllers/IndexController.php
@@ -2,11 +2,25 @@
 
 namespace Apiphalcon\Frontend\Controllers;
 
+//use QL\QueryList;
+use Phalcon\Mvc\View;
+use Phalcon\Loader;
+
 class IndexController extends ControllerBase
 {
 
     public function indexAction()
     {
+        $loader = new Loader();
+        $loader->registerNamespaces(
+            [
+                'QL/QueryList'    => 'vendor/QL/QueryList',
+            ]
+        );
+        $loader->register();
+        $QueryList = new \QL\QueryList();
+        $data = $QueryList->get('http://cms.querylist.cc/bizhi/453.html')->find('img')->attr('src');
+//        var_dump($data->all());
 
     }
 
